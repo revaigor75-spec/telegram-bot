ab7185fa399d106831a2242898664e1d808e107c
revaigor75-spec <revaigor75@gmail.com>
2026-05-18 11:20:43 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,74 @@
+from telethon import TelegramClient, events
+import re
+
+api_id = 36239085
+api_hash= "963590c1d6bdcc8dc6995fb6b01aee1f"
+
+SOURCE_CHAT_NAME = "DUCK × MY × DUCK | Duckers group"
+TARGET_CHANNEL = "epicsqwer132"
+
+KEYWORDS = ["rare", "epic"]
+
+client = TelegramClient("session", api_id, api_hash)
+
+
+@client.on(events.NewMessage)
+async def handler(event):
+
+    chat = await event.get_chat()
+
+    if getattr(chat, "title", "") != SOURCE_CHAT_NAME:
+        return
+
+    text = event.raw_text.lower()
+
+    print("Новое сообщение:")
+    print(text)
+
+    # ссылка
+    link_match = re.search(r'https?://\S+', text)
+
+    if not link_match:
+        return
+
+    link = link_match.group(0)
+
+    result = None
+
+    # ищем rare / epic + число
+    for word in KEYWORDS:
+
+        pattern = rf'\b{word}\b\s*([1-4])?'
+
+        match = re.search(pattern, text)
+
+        if match:
+
+            level = match.group(1)
+
+            if level:
+                result = f"{word} {level}"
+            else:
+                result = word
+
+            break
+
+    if not result:
+        return
+
+    final_message = f"{result}\n{link}"
+
+    await client.send_message(
+        TARGET_CHANNEL,
+        final_message,
+        link_preview=False
+    )
+
+    print("Отправлено:")
+    print(final_message)
+
+
+print("Бот запущен")
+
+client.start()
+client.run_until_disconnected()
